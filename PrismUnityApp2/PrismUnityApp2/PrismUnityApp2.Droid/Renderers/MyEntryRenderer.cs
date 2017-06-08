df37d72d082c1d129ce2f6d703368707d1a5da9e
easonchang <gokart8888@gmail.com>
2017-06-08 11:09:51 +0800

    客製Entry

---
+++ PrismUnityApp2/PrismUnityApp2/PrismUnityApp2.Droid/Renderers/MyEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using PrismUnityApp2.Controls;
+using Xamarin.Forms.Platform.Android;
+using PrismUnityApp2.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace PrismUnityApp2.Droid.Renderers
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        MyEntry myEntry;
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                myEntry = e.NewElement as MyEntry;
+            }
+
+            if (Control != null)
+            {
+                EditText textField = (EditText)Control;
+                //textField.Typeface = Android.Graphics.Typeface.Monospace;
+                var color = Android.Graphics.Color.ParseColor(myEntry == null ? "#c8c8c8" : myEntry.PlaceholderColorText);
+                textField.SetHintTextColor(color);
+            }
+        }
+    }
+}
---
+++ PrismUnityApp2/PrismUnityApp2/PrismUnityApp2/Controls/MyEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrismUnityApp2.Controls
+{
+    public class MyEntry : Entry
+    {
+        public static readonly BindableProperty PlaceHolderColorProperty = BindableProperty.Create("PlaceholderColorText", typeof(string), typeof(MyEntry), "#cccccc");
+
+        public string PlaceholderColorText
+        {
+            get { return (string)GetValue(PlaceHolderColorProperty); }
+            set { SetValue(PlaceHolderColorProperty, value); }
+        }
+    }
+}
